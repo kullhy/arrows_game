7138fff743ac7d9fa1ffba558004f35495f5e8cb
Bator <robbator@gmail.com>
2026-02-20 21:17:45 +0100

    rel: 1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.batodev.arrows"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
