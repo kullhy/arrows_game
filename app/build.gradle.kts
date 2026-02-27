f0bbc45f154ea58e46f1e1d8f3991948726ca0a3
robmat <support@emberfox.online>
2026-02-27 08:44:19 +0100

    rel: 1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.batodev.arrows"
         minSdk = 29
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
